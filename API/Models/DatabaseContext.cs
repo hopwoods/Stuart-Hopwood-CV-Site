b7abb76caa5e8d74b43f09ff9a16cb3fb6eca6e8
Stuart Hopwood <stoo.hopwood@gmail.com>
2020-12-08 11:50:36 +0000

    Add API Project

---
+++ API/Models/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+            option
+        }
+
+        public DbSet<CvSection> CvSections { get; set; }
+    }
+}
